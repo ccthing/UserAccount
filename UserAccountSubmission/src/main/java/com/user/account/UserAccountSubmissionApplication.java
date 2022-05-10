8efa9b3f0c950f4592c0baba76071266336cb135
ThinG <ThinG@DESKTOP-TSE11M0>
2022-05-10 16:27:19 +0800

    Commit all files

---
+++ UserAccountSubmission/src/main/java/com/user/account/UserAccountSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.user.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccountSubmissionApplication.class, args);
+	}
+
+}
